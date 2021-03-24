e624ae8f56c4d5cf6f389e8ac761b32519d13b04
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2021-03-24 20:00:17 +0300

    Squares

--- Problems/Squares of natural numbers/src/Main.java
+++ Problems/Squares of natural numbers/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int i = 1;
+        while(true) {
+            int square = i * i;
+            if (square <= n) {
+                System.out.println(square);
+                i++;
+            } else {
+                break;
+            }
+        }
     }
 }
